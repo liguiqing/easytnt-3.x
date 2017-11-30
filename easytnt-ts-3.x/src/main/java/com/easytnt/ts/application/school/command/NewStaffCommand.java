12a24ae01ec80c8fa4d91546ce61e12cbcc3fbc8
ligq <31333250@qq.com>
2017-11-30 17:41:59 +0800

    增加学校安排教职工方法

---
+++ easytnt-ts-3.x/src/main/java/com/easytnt/ts/application/school/command/NewStaffCommand.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2016,2017, easytnt All Rights Reserved. 深圳市易考试乐学测评有限公司 版权所有.
+ */
+
+package com.easytnt.ts.application.school.command;
+
+/**
+ * @author Liguiqing
+ * @since V3.0
+ */
+
+public class NewStaffCommand {
+
+
+}
