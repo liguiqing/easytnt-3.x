a0e77b4659879afc1e669f8517ed40d4f0fc79bf
liguiqing <liguiking@gmail.com>
2017-12-05 00:01:16 +0800

    更新SchoolApplicationService

--- easytnt-ts-3.x/src/main/java/com/easytnt/ts/domain/model/school/common/IdentityType.java
+++ easytnt-ts-3.x/src/main/java/com/easytnt/ts/domain/model/school/common/IdentityType.java
@@ -13,7 +13,7 @@ package com.easytnt.ts.domain.model.school.common;
 
 public enum IdentityType {
     IDCard("身份证", 1), StudyNumber("学籍号", 2), SchoolNumber("学号", 3),EduID("教育云标识",4),
-    QQ("QQ号",5),Weixin("微信",6), HKMATWIDCard("港澳台证件号", 7), JobNo("考号", 8),Other("其他", 99);
+    QQ("QQ号",5),Weixin("微信",6), HKMATWIDCard("港澳台证件号", 7), JobNo("考号", 8),Other("其他", 99),Systems("系统生成",100);
 
     private final String name;
 
