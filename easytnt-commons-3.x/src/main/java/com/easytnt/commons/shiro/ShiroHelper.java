a7476c5bb57597acfd8f18d7d42a98b0829980de
ligq <31333250@qq.com>
2018-01-05 21:23:14 +0800

    增加shiro辅助类

---
+++ easytnt-commons-3.x/src/main/java/com/easytnt/commons/shiro/ShiroHelper.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016,2018, easytnt All Rights Reserved. 深圳市易考试乐学测评有限公司 版权所有.
+ */
+
+package com.easytnt.commons.shiro;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * http　port的超类
+ * @author Liguiqing
+ * @since V3.0
+ */
+
+public  class ShiroHelper {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public static  Subject getSubject(){
+        Subject subject = SecurityUtils.getSubject();
+
+        if(subject.isRunAs()) {
+            subject.releaseRunAs();
+        }
+
+        return subject;
+    }
+
+}
