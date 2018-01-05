22e8eb893f034006805ff2a887dc1a067ebfe5b7
ligq <31333250@qq.com>
2018-01-05 21:30:00 +0800

    增加http port

---
+++ easytnt-ts-3.x/src/main/java/com/easytnt/ts/port/adapter/http/controller/OutApiController.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2016,2018, easytnt All Rights Reserved. 深圳市易考试乐学测评有限公司 版权所有.
+ */
+
+package com.easytnt.ts.port.adapter.http.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Liguiqing
+ * @since V3.0
+ */
+
+@RequestMapping(value = "/ts/api")
+public class OutApiController extends AbstractTSControllerHelper {
+
+
+}
